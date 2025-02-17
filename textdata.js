514d10bab7471c317c76740041724e207ac200b0
© ᴍʀ-ʟᴀᴋᴀ 🧸🖇️✨ <lakaofc@gmail.com>
2025-02-17 08:36:45 +0530

    Update textdata.js

--- textdata.js
+++ textdata.js
@@ -1,2 +1,12 @@
 {
+"footer": "*Followe Us*\nChanel:\nsc:https://github.com/lakaofc\n> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʟᴀᴋᴀ-ᴍᴅ 🧘‍♂️🪄🖇️"
+"replyMsg": {
+  "givetext": "*Please give me text..!✏️*",
+  "giveurl": "*Please give me Url..!🖇️*",
+  "cowner": "*Owner Only Comade..!🪄*",
+  "gadm": "*Give Me Group Admin..!🫣*",
+  "gonly": "*This Comade Only Group..!👥*",
+  "pinbox": "*This Comade Only Use Inbox..!👁️*",
+  "emsg": "*Erorr..!*"
+
 }
