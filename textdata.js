56973fb02e69e36ec6dd6a178a8c9da6409ff196
© ᴍʀ-ʟᴀᴋᴀ 🧸🖇️✨ <lakaofc@gmail.com>
2025-02-25 00:47:06 +0530

    Update textdata.js

--- textdata.js
+++ textdata.js
@@ -1,5 +1,6 @@
 {
 "footer": "*● Fᴏʟʟᴏᴡᴇ Uꜱ 🔥*\n*REPO 🎲*\nhttps://github.com/lakaofc/laka-md\n\n> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʟᴀᴋᴀ-ᴍᴅ 🪶",
+"mainimg": "https://i.ibb.co/GBBGm1t/21.jpg",
 "replyMsg": {
   "givetext": "*Please give me text..!✏️*",
   "giveurl": "*Please give me Url..!🖇️*",
